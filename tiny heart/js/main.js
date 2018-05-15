3b4e65e7cba409fd638ad1eb5ff9fb0dc0f076ee
cangengchen <2546173476@qq.com>
2018-05-15 15:34:53 +0800

    第一次上传
    提交初始准备代码、图片

---
+++ tiny heart/js/main.js
@@ -0,0 +1,21 @@
+var can1;
+var can2;
+
+var ctx1;
+var ctx2;
+
+
+document.body.onload = game;
+function game()
+{
+	init();
+}
+
+function init()
+{
+	//获得canvas context
+	can1 = document.getElementById('canvas1');//fishes,dust,UI,circle
+	ctx1 = can1.getContext();
+	can2 = document.getElementById('canvas2');//background,ane,fruits
+	ctx2 = can2.getContext();
+}
