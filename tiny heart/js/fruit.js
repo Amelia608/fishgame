ac8d17507d7eb82c9f0a3259d25d84dd4164c13f
cangengchen <2546173476@qq.com>
2018-05-16 16:56:02 +0800

    爱心鱼（上）完成

--- tiny heart/js/fruit.js
+++ tiny heart/js/fruit.js
@@ -3,6 +3,9 @@ var fruitObj = function()
 	this.alive = []; //bool
 	this.x = [];
 	this.y = [];
+	this.l = [];
+	this.spd = [];
+	this.fruitType = [];
 	this.orange = new Image();
 	this.blue = new Image();
 }
@@ -11,10 +14,11 @@ fruitObj.prototype.init = function()
 {
 	for(var i=0;i < this.num; i++)
 	{
-		this.alive[i] = true;
+		this.alive[i] = false;
 		this.x[i] = 0;
 		this.y[i] = 0;
-		this.born(i);
+		this.spd[i] = Math.random() * 0.017 + 0.003;//[0.0053,0.02)
+		this.fruitType[i] = "";
 	}
 	this.orange.src = "./src/fruit.png";
 	this.blue.src = "./src/blue.png"
@@ -25,7 +29,32 @@ fruitObj.prototype.draw = function()
 	{
 		//draw
 		//find an ane,grow,fly up
-		ctx2.drawImage(this.orange,this.x[i] - this.orange.width * 0.5,this.y[i]-this.orange.height * 0.5);
+		if(this.alive[i])
+		{
+			if(this.fruitType[i] == "blue")
+			{
+				var pic = this.blue;
+			}
+			else
+			{
+				var pic = this.orange;
+			}
+			if(this.l[i] <= 14)
+				{
+					this.l[i] += this.spd[i] * deltaTime;
+				}
+			else
+			{
+				this.y[i] -= this.spd[i] * 7 * deltaTime;
+			}
+		
+			ctx2.drawImage(pic,this.x[i] - this.l[i] * 0.5,this.y[i]-this.l[i] * 0.5,this.l[i],this.l[i]);
+			if(this.y[i] < 10)
+			{
+				this.alive[i] = false;
+			}
+		}
+		
 	}
 }
 fruitObj.prototype.born = function(i)
@@ -33,6 +62,22 @@ fruitObj.prototype.born = function(i)
 	var aneID = Math.floor(Math.random() * ane.num);
 	this.x[i] = ane.x[aneID];
 	this.y[i] = canHeight - ane.len[aneID];
+	this.l[i] = 0;
+	this.alive[i] = true;
+	var ran = Math.random();
+	if(ran < 0.2)
+	{
+		this.fruitType[i] = "blue";
+	}
+	else
+	{
+		this.fruitType[i] = "orange";//orange,blue
+	}
+	
+}
+fruitObj.prototype.dead = function(i)
+{
+	this.alive[i] = false;
 }
 fruitObj.prototype.update = function()
 {
@@ -40,5 +85,30 @@ fruitObj.prototype.update = function()
 	for(var i=0;i < this.num;i++)
 	{
 		if(this.alive[i]) num++;
+	}
+}
+function fruitMonitor()
+{
+	var num = 0;
+	for(var i = 0; i<fruit.num;i++)
+	{
+		if(fruit.alive[i]) num++;
+	}
+	if(num < 15)
+	{
+		//send fruit
+		sendFruit();
+		return;
+	}
+}
+function sendFruit()
+{
+	for(var i = 0;i<fruit.num; i++)
+	{
+		if(!fruit.alive[i])
+		{
+			fruit.born(i);
+			return;
+		}
 	}
 }
--- tiny heart/js/main.js
+++ tiny heart/js/main.js
@@ -15,6 +15,13 @@ var bgPic = new Image();
 var ane;
 var fruit;
 
+var mom;
+var baby;
+
+var mx;
+var my;
+
+
 document.body.onload = game;
 function game()
 {
@@ -32,6 +39,8 @@ function init()
 	can2 = document.getElementById('canvas2');//background,ane,fruits
 	ctx2 = can2.getContext('2d');
 
+	can1.addEventListener('mousemove',onMouseMove,false);
+
 	bgPic.src = "./src/background.jpg";
 
 	canWidth = can1.width;
@@ -42,6 +51,15 @@ function init()
 
 	fruit = new fruitObj();
 	fruit.init();
+
+	mom = new momObj();
+	mom.init();
+
+	baby = new babyObj();
+	baby.init();
+
+	mx =  canWidth * 0.5;
+	my =  canHeight * 0.5;
 }
 function gameloop()
 {
@@ -49,8 +67,23 @@ function gameloop()
 	var now = Date.now();
 	deltaTime = now -lastTime;
 	lastTime = now;
+	if(deltaTime > 40) deltaTime = 40;
 
 	drawBackground();
 	ane.draw();
+	fruitMonitor();
 	fruit.draw();
+
+	ctx1.clearRect(0,0,canWidth,canHeight);
+	mom.draw();
+	momFruitsCollision();
+	baby.draw();
+}
+function onMouseMove(e)
+{
+	if(e.offSetX || e.layerX)
+	{
+		mx = e.offSetX == undefined ? e.layerX : e.offSetX;
+		my = e.offsetY == undefined ? e.layerY : e.offsetY;
+	}
 }
